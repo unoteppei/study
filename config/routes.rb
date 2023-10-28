7537ac2e27b6583efb3e343fb9b30c36f8547e65
unoteppei <143475813+unoteppei@users.noreply.github.com>
2023-10-29 08:31:32 +0900

    ユーザー編集パス追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
+  # get
+  get '/dashboard', to: 'users#dashboard'
+  get 'pages/home'
+  # post
+  post '/users/edit', to: 'users#update'
+  
+  devise_for :users
+  
 end
