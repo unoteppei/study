4fefe228df55d9a2f9395110e21b45a6c47bb816
unoteppei <143475813+unoteppei@users.noreply.github.com>
2023-11-18 08:36:22 +0900

    プロフィール詳細メソッド追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,11 @@ class UsersController < ApplicationController
     end
     redirect_to dashboard_path
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   private
   def current_user_params
     params.require(:user).permit(:about, :status, :avatar)
